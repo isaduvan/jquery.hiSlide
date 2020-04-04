9bf7d59754ca6995d9ccbc1b99ec8a322cc0df8d
isaduvan <isaduvan@outlook.com>
2020-04-04 19:39:46 +0300

    update jquery.hislide.js
    it will now work for more than seven images and will not throw errors.

--- js/jquery.hislide.js
+++ js/jquery.hislide.js
@@ -23,7 +23,8 @@
             { $zIndex: 4, width: 224, height: 288, top: 0, left: 263, $opacity: 1 },
             { $zIndex: 3, width: 170, height: 218, top: 35, left: 470, $opacity: 0.7 },
             { $zIndex: 2, width: 130, height: 170, top: 59, left: 620, $opacity: 0.4 },
-            { $zIndex: 1, width: 120, height: 150, top: 69, left: 500, $opacity: 0.2 }
+            { $zIndex: 1, width: 120, height: 150, top: 69, left: 500, $opacity: 0.2 },
+            { $zIndex: 0, width: 0, height: 0, top: 146, left: 377, $opacity: 0 }
         ];
 
         var $lis = $ele.find('li');
@@ -51,6 +52,13 @@
         // 让 li 从正中间展开
         function move() {
             $lis.each(function(index, element) {
+                var state ;
+                if(index>states.length-1){
+                    state = states[8]
+                }
+                else{
+                    state = states[index];
+                }
                 var state = states[index];
                 $(element).css('zIndex', state.$zIndex).finish().animate(state, setting.speed).find('img').css('opacity', state.$opacity);
             });
